42951f37c50cd98dbcd9afae5f3e932ee08db1f7
alexey02rus <101921891+alexey02rus@users.noreply.github.com>
2022-10-06 21:59:09 +0300

    Добавьте файлы проекта.

---
+++ Task1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product("Бананы", 90, @"Image\banan.jpg", ProdCateg.Еда));
+            products.Add(new Product("Утюг", 2100, @"Image\iron.jpg", ProdCateg.Техника));
+            products.Add(new Product("Масло сливочное", 150, @"Image\butter.jpg", ProdCateg.Еда));
+            products.Add(new Product("Цыпленок", 250, @"Image\kuritsa.jpg", ProdCateg.Еда));
+            products.Add(new Product("Чайник", 1500, @"Image\kettle.jpg", ProdCateg.Техника));
+            listBox.ItemsSource = products;
+        }
+    }
+}
---
+++ Task1/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public enum ProdCateg
+    {
+        Еда,
+        Техника
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public ProdCateg ProdCateg { get; set; }
+
+        public Product(string name, double price, string image, ProdCateg prodCateg)
+        {
+            Name = name;
+            Price = price;
+            Image = image;
+            ProdCateg = prodCateg;
+        }
+    }
+}
